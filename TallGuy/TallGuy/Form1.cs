cd51e6b34ec98acd5150237ec54b48cb1c0a6332
InFar <infar45@gmail.com>
2018-03-13 20:02:06 +0300

    Add TallGuy project

---
+++ TallGuy/TallGuy/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TallGuy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        interface IClown
+        {
+            string FunnyThingIHave { get; }
+            void Honk();
+        }
+
+        class TallGuy : IClown 
+        {
+            public string Name;
+            public int Height;
+
+            public void TalkAboutYourself()
+            {
+                MessageBox.Show("My name is " + Name + " my height is " + Height + " sm.");
+            }
+
+            public string FunnyThingIHave
+            {
+                get { return "Big boots"; }
+            }
+
+            public void Honk()
+            {
+                MessageBox.Show("bi!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TallGuy tallGuy = new TallGuy() { Height = 190, Name = "Jimm" };
+            tallGuy.TalkAboutYourself();
+        }
+    }
+}
